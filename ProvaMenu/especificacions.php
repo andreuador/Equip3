67d3086c1d3979e79deaf024a3e6a02d5bafe930
javieramosfuentes <120259908+javieramosfuentes@users.noreply.github.com>
2023-09-28 11:11:17 +0200

    Add files via upload

--- ProvaMenu/especificacions.php
+++ ProvaMenu/especificacions.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <title>Especificacions</title>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <h1>Especificacions</h1>
-    </body>
+<head>
+    <title>Especificacions</title>
+    <meta charset="utf-8">
+</head>
+<body>
+<h1>Especificacions</h1>
+</body>
 </html>
