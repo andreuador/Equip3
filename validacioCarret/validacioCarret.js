c58484e786146b8447826baf5c9cf69817a44d36
javieramosfuentes <120259908+javieramosfuentes@users.noreply.github.com>
2023-09-29 13:31:47 +0200

    actualització de el preu total de la compra

--- validacioCarret/validacioCarret.js
+++ validacioCarret/validacioCarret.js
@@ -1,17 +1,20 @@
 "use strict";
 
-
 function main(){
     let table       = document.querySelector('body>table'); //Seleccionem la taula del document html
     let imatges     = ['img/rollsRoyce.jpeg','img/ferrari.jpeg','img/buggati.jpeg','img/maserati.webp','img/mercedes.jpeg','img/porsche.jpeg'];
+    let marques     = ['Rolls Royce','Ferrari','Buggati','Maserati','Mercedes','Porsche'];
     let preuCarret  = document.querySelector('#preuCarret>h1'); 
     let carret = [];
     let id = 1;
+    let totalCarret = 0;
 
     createThs(table);
     createTds(table);
     createTds(table);
     createTds(table);
+
+    preuCarret.textContent = totalCarret;
 
     /**
      * Mètode que crea els títols de cada cel·la de la taula
@@ -40,6 +43,15 @@ function main(){
         img.setAttribute('src',imatges[parseInt(Math.random()*imatges.length)]);
         img.setAttribute('width','100px');
 
+        let vehicle = {
+            image:img,
+            nom:marques[parseInt(Math.random()*marques.length)],
+            import:importe,
+            cantitat:cantitat,
+        }
+
+        calculaTotalCarret(vehicle.import);
+
         let div = document.createElement('div');                
         let buttonDel = document.createElement('button');       //Botó per eliminar el producte del carret
         buttonDel.setAttribute('id',id);
@@ -51,63 +63,65 @@ function main(){
         let buttonSuma = document.createElement('button');      //Botó per incrementar la cantitat de vehicles 
         buttonSuma.innerHTML = "+";
 
+
         div.append(buttonResta);
-        div.append(cantitat);
+        div.append(vehicle.cantitat);
         div.append(buttonSuma);
 
-        createTd("imagenV",img,tr);
-        createTd("nombreV","Coche",tr);
-        createTd("importe",importe+"$",tr);
-        createTd("cantidad",div,tr);
-        createTd("total",calculaTotalProducte(cantitat,importe)+"$",tr);
-        createTd("del",buttonDel,tr);
+        
 
-        let vehicle = {
-            image:img,
-            nom:"coche",
-            import:importe,
-            cantitat:cantitat,
-        }
+        createTd("imagenV",vehicle.image,tr);
+        createTd("nombreV",vehicle.nom,tr);
+        createTd("importe",vehicle.import,tr);
+        createTd("cantidad",div,tr);
+        createTd("total",calculaTotalProducte(vehicle.cantitat,vehicle.import),tr);
+        createTd("del",buttonDel,tr);
 
         carret.push(vehicle);
         localStorage.setItem('vehicle'+id,vehicle);
-        preuCarret.textContent = calculaTotalCarret();
-
+        
+        
         table.append(tr);                                       //S'anyadeix el la fila a la taula
         id += 1;
+
+        
 
         /**
          * Aquest event fa que quan es clicke en el botó de suma incremente la cantitat del mateix producte 
          */
         buttonSuma.addEventListener('click',function(){
-            cantitat++;
+            vehicle.cantitat++;
             div.textContent = "";
             div.append(buttonResta);
-            div.append(cantitat);
+            div.append(vehicle.cantitat);
             div.append(buttonSuma);
 
             let total = tr.querySelector('.total');
             total.textContent = "";
-            total.textContent = calculaTotalProducte(cantitat,importe)+"$";
+            total.textContent = calculaTotalProducte(vehicle.cantitat,vehicle.import);
 
             preuCarret.textContent = "";
-            preuCarret.textContent = calculaTotalProducte(cantitat,importe)+"$";
+            preuCarret.textContent = calculaTotalCarret(vehicle.import);
+
         });
 
         /**
          * Aquest event fa que quan es clicke en el botó de resta disminueixca la cantitat del mateix producte 
          */
         buttonResta.addEventListener('click',function(){
-            if(cantitat > 1){
-                cantitat--;
+            if(vehicle.cantitat > 1){
+                vehicle.cantitat--;
                 div.textContent = "";
                 div.append(buttonResta);
-                div.append(cantitat);
+                div.append(vehicle.cantitat);
                 div.append(buttonSuma);
 
                 let total = tr.querySelector('.total');
                 total.textContent = "";
-                total.textContent = calculaTotalProducte(cantitat,importe)+"$";
+                total.textContent = calculaTotalProducte(vehicle.cantitat,vehicle.import);
+
+                preuCarret.textContent = "";
+                preuCarret.textContent = resta(vehicle.import);
             }
         });
 
@@ -118,8 +132,11 @@ function main(){
             tr.remove(tr);
             
             localStorage.removeItem('vehicle'+buttonDel.getAttribute('id'));
-            if(buttonDel.getAttribute('id') == id)
+            if(buttonDel.getAttribute('id') == id){
                 localStorage.removeItem('vehicle'+id);
+            }
+            preuCarret.textContent = "";
+            preuCarret.textContent = resta(calculaTotalProducte(vehicle.cantitat,vehicle.import));
 
         });
     }
@@ -127,8 +144,8 @@ function main(){
     /**
     * Aquest mètode s'encarrega de calcular l'import total del producte 
     */
-    function calculaTotalProducte(cantitat, importe){
-        return cantitat*importe;
+    function calculaTotalProducte(cant, imp){
+        return cant*imp;
     }
 
     /**
@@ -150,13 +167,16 @@ function main(){
         tr.append(td);
     }
 
-    function calculaTotalCarret(){
-        let suma=0;
-        for(let i = 0;i<carret.length;i++){
-            suma += carret[i].cantitat;
-        };
-        return suma;
+    function calculaTotalCarret(imp){
+        return totalCarret += imp;
+      
     }
+
+    function resta(imp){
+        return totalCarret -= imp;
+    }
+    
 }
+
 
 document.addEventListener('DOMContentLoaded',main());
