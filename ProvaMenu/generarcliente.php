a6e7db458f0e120d1ff710d46644e7758abbd6d6
Jorditin <76152011+Jorditin@users.noreply.github.com>
2023-09-29 19:32:25 +0200

    Update generarcliente.php

--- ProvaMenu/generarcliente.php
+++ ProvaMenu/generarcliente.php
@@ -3,18 +3,38 @@
 
 <html>
     <body>
+        
         <?php 
         
             require 'Cliente.php';
             $client = new Cliente();
                
         ?>
-
+<div>
+    <button onclick="desplegar()">Usuario</button>
+    <div id="enlaces" style="display: none;">
+      <a href="./generarcliente.php">Información Usuario</a><br>
+      <a href="./cataleg.php">Cataleg</a><br>
+      <a href="./carrito.php">Carrito</a><br>
+    </div>
+  </div>
+        
         <p><?php echo "Nombre:". $client->generarNom() ?></p>
         <p><?php echo "Usuario:".$client->generarUsuari() ?></p>
         <p><?php echo "Email:".$client->generarEmail() ?></p>
         <p><?php echo "DNI:".$client->generarDNI() ?></p>
         <p><?php echo "Direcció:".$client->generarDireccio() ?></p>
         <p><?php echo "Targeta:".$client->generarTargeta() ?></p>
+    
+        <script>
+  function desplegar() {
+    var x = document.getElementById("enlaces");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+      </script>
 </body>
 </html>
